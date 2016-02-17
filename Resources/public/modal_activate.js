26a35cf3ef3ed6acdb80d525a01ced01c18675d2
stephylee <slemoine@slad.eu>
2016-02-17 17:33:43 +0100

    Create modal_activate.js

---
+++ Resources/public/modal_activate.js
@@ -0,0 +1,34 @@
+/*!
+* jQuery modal activation
+*
+* Copyright 2016, Slad
+*/
+
+(function ($) {
+   
+        // gestion du remplissage et de l'ouverture de la fenetre modal 
+        $('#dayModal').on('show.bs.modal', function (event) {           
+            var button = $(event.relatedTarget); 
+
+            var recipient = button.data('whatever'); 
+            result 	= recipient.split("--");
+            var modal = $(this);
+            url =  Routing.generate('day_index', { _locale: locale }),
+            $.ajax({
+                type : 'POST',
+                data : { date: result[0], 
+                         item: result[1]
+                         },
+                url : url,
+               success : function(code_html, statut){ 
+                   modal.find('.modal-body').html(code_html);
+                   code_html='';
+               },
+               error : function(resultat, statut, erreur){
+                   alert('error');  // messsage a personnaliser
+               }
+            });
+            modal.find('#dayModalLabel').text('translations.bookday' + result[0]);
+        });
+
+})(jQuery);
