f0dc94f2fd465b02de203ff6f6717db4fe01c227
stephylee <slemoine@slad.eu>
2016-03-07 15:59:10 +0100

    Create ClassTwigExtension.php

---
+++ Twig/ClassTwigExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Slad\BookingBundle\Twig;
+
+class ClassTwigExtension extends \Twig_Extension
+{
+    public function getFunctions()
+    {
+        return array(
+            'class' => new \Twig_SimpleFunction('class', array($this, 'getClass'))
+        );
+    }
+
+    public function getName()
+    {
+        return 'class_twig_extension';
+    }
+
+    public function getClass($object)
+    {
+        return (new \ReflectionClass($object))->getShortName();
+    }
+}
